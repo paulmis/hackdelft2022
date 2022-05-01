7dd1238dbfd3e61c40c269f5c33a8e4da4326c86
Aleksandra Andrasz <a.andrasz@student.tudelft.nl>
2022-05-01 12:22:08 +0200

    predict

--- test123/main.py
+++ test123/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 from trends import trends
+from pct_change_trends import trends as pct_trends
 from terms import terms
 import seaborn as sn
 
@@ -14,16 +15,16 @@ with jsonlines.open('data.jl') as reader:
 normalized = []
 for entry in terms:
     normalized.append([["asylum " + entry['language'] + " " + entry['nationality']],
-                       trends(gov, 'asylum', entry['language'], entry['asylum'], entry['code'], entry['nationality'])])
+                       pct_trends(gov, 'asylum', entry['language'], entry['asylum'], entry['code'], entry['nationality'])])
     normalized.append([["war " + entry['language'] + " " + entry['nationality']],
-                       trends(gov, 'war', entry['language'], entry['war'], entry['code'], entry['nationality'])])
+                       pct_trends(gov, 'war', entry['language'], entry['war'], entry['code'], entry['nationality'])])
     if 'netherlands' in entry:
         normalized.append([["netherlands " + entry['language'] + " " + entry['nationality']],
-                           trends(gov, 'netherlands', entry['language'], entry['netherlands'], entry['code'],
+                           pct_trends(gov, 'netherlands', entry['language'], entry['netherlands'], entry['code'],
                                   entry['nationality'])])
     if 'taliban' in entry:
         normalized.append([["taliban " + entry['language'] + " " + entry['nationality']],
-                           trends(gov, 'taliban', entry['language'], entry['taliban'], entry['code'],
+                           pct_trends(gov, 'taliban', entry['language'], entry['taliban'], entry['code'],
                                   entry['nationality'])])
 
 normalized.sort(key=lambda e: e[1])
---
+++ test123/pct_change_trends.py
@@ -0,0 +1,57 @@
+import datetime
+import re
+import calendar
+import matplotlib.pyplot as plt
+import numpy
+import pandas as pd
+from pytrends.request import TrendReq
+import seaborn as sn
+import numpy as np
+
+
+month_to_nr = list(calendar.month_name)
+
+
+def trends(gov, eng_term, lang, term, country, nationality):
+    pytrends = TrendReq()
+    kw_list = [term]
+
+    print(eng_term, lang, country)
+    pytrends.build_payload(kw_list, cat=0, timeframe='2013-01-01 2022-03-31', geo=country, gprop='')
+    google = pytrends.interest_over_time()[term]
+    google = google.rename("searches").pct_change().rolling(7).sum()
+
+    gov_2 = [[entry['TotalAsylumRequests_1'], entry['Periods']] for entry in gov
+             if entry['Nationality'] == nationality
+             and re.match("^(\w*) (\w*)$", entry['Periods'])
+             and entry['Age'] == 'Total'
+             and entry['Sex'] == 'Total male and female ']
+    gov_3 = [[entry[0], re.split("^(\w*) (\w*)$", entry[1])[1:3]] for entry in gov_2]
+    gov_4 = [[entry[0], [int(entry[1][0]), month_to_nr.index(entry[1][1])]] for entry in gov_3]
+    gov_5no = [entry[0] for entry in gov_4]
+    gov_5date = [(datetime.datetime(entry[1][0], entry[1][1], 1) - datetime.timedelta(days=1)).replace(day=1) for entry in gov_4]
+    gov_pd = pd.Series(data=gov_5no, index=gov_5date, name='requests')
+    gov_pd_change = gov_pd.pct_change().rolling(7).sum()
+    
+    d = np.arange(len(gov_pd))
+    A = np.vstack([d, np.ones(len(gov_pd))])
+    a, b = np.linalg.lstsq(A.T, gov_pd.values, rcond=None)[0]
+    
+    arr = (a*d + np.full(d.shape, b))*google
+    plt.plot(d, gov_pd.values)
+    plt.plot(d, a*d + b, 'r')
+    plt.title("Base line" + nationality)
+    plt.xlabel("Date")
+    plt.ylabel("Applications")
+    plt.show()
+    
+    print(a)
+
+    info_string = eng_term + " " + lang + " " + " " + nationality
+
+    data_all = pd.concat([google[:-1], gov_pd[1:]], axis=1)
+    norma_all = (data_all - data_all.mean()) / data_all.std()
+    norma_all.plot().set_xlabel(info_string)
+    plt.savefig("graphs/" + info_string + ".jpg")
+
+    return norma_all.corr().searches.requests
